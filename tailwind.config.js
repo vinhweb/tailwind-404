95bfab0e4bdb61c85235cc79564488b3f724cfb0
Vinh <vjhnguyen@gmail.com>
2022-03-09 07:34:06 +0700

    404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
