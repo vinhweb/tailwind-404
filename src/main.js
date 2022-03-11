e84657e6aa3937c816f09ceacb127ad5b0cd3797
Vinh <vjhnguyen@gmail.com>
2022-03-12 05:01:12 +0700

    faq page

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const listItems = document.querySelectorAll('#faq_list li');
+
+listItems.forEach(item => {
+    const plusBtn = item.querySelector('button .plus');
+    const minusBtn = item.querySelector('button .minus');
+
+    plusBtn.addEventListener('click', function(){
+        removeClassFAQ();
+        item.classList.add('active');
+    })
+
+    minusBtn.addEventListener('click', function(){
+        removeClassFAQ();
+    })
+})
+
+function removeClassFAQ(){
+    const listActiveItems = document.querySelectorAll('#faq_list li.active');
+    listActiveItems.forEach(item => item.classList.remove('active'));
+}
